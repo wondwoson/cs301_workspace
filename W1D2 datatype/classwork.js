7f6b908ed70daee6b57f7f5f5c3ef5c63d420dc9
wondwoson <wongotera2014@gmail.com>
2021-02-24 20:09:58 -0600

    repetition

---
+++ W1D2 datatype/classwork.js
@@ -0,0 +1,13 @@
+const prompt=require("prompt-sync")();
+
+let weather=prompt("enter the weather today: ");
+let number=Math.random()10);
+if(weather==="rainy"){
+    console.log("get umberela");
+}
+if(number===7){
+    console.log("BINGO!");
+}
+else{
+    console.log("try Again");
+}
---
+++ W1D3/weather.js
@@ -0,0 +1,13 @@
+const prompt=require("prompt-sync")();
+let weather=prompt("enter weather type today");
+let footwear=" ";
+if(weather==="hot"){
+    footwear="sandal";
+}else if(weather===rain){
+    footwear="galoshis";
+}
+else if(weather===snow){
+    footwear=boots;
+}else{
+    footwear="sneaker";
+}
