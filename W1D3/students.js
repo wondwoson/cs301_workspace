454503fc560c253bbb0f0914b31551c259b9a508
wondwoson <wongotera2014@gmail.com>
2021-02-24 22:00:12 -0600

    question3

---
+++ W1D3/students.js
@@ -0,0 +1,17 @@
+
+const prompt=require("prompt-sync")();
+let credeit=prompt("enter crediet hour of the course ");
+let name=prompt("enter the name of the student ")
+let label;
+if(credeit>90){
+label="Senior";
+}else if(credeit>60){
+label="Junior";    
+}else if(credeit>30){
+label="Sophomor";   
+}else if(credeit>0){
+label="Freshman";    
+}
+console.log("the stdents name is:" +name);
+console.log("the stdents label is: "+label);
+
