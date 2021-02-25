891349af0e88b2912fe281f4ce423574c1a6d062
wondwoson <wongotera2014@gmail.com>
2021-02-24 20:40:48 -0600

    reptetion

--- W1D3/weather.js.js
+++ W1D3/weather.js.js
@@ -1,13 +1,14 @@
 const prompt=require("prompt-sync")();
-let weather=prompt("enter weather type today");
+let weather=prompt("enter weather type today :");
 let footwear=" ";
 if(weather==="hot"){
     footwear="sandal";
-}else if(weather===rain){
+}else if(weather==="rain"){
     footwear="galoshis";
 }
-else if(weather===snow){
+else if(weather==="snow"){
     footwear=boots;
 }else{
     footwear="sneaker";
 }
+console.log(footwear);
